a4b997e41a3c784be6f27fc633b817a7ba0e72d8
Daniel Alexis Moreno Herrera <daniel10697@hotmail.com>
2021-11-17 02:52:57 +0100

    Test Numero 4 Funcional

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -26,14 +26,32 @@ class WeatherTest extends TestCase
     /** @test */
     public function find_the_weather_of_any_day()
     {
-        $client = new GuzzleHttpClient();
 
+	    $client = new GuzzleHttpClient();    
+	    
         $forecast = new ForecastWeather($client);
         $city = "Madrid";
 
         $prediction = $forecast->predict($city, new \DateTime('+2 days'));
 
         echo "Day after tomorrow: $prediction\n";
+        $this->assertTrue(true, 'I don\'t know how to test it');
+    }
+
+    /** @test */
+    public function find_the_wind_of_any_day()
+    {
+
+        $client = new GuzzleHttpClient();
+            
+        $forecast = new ForecastWeather($client);
+        $city = "Madrid";
+
+        $noDate = null;
+
+        $prediction = $forecast->predict($city, $noDate);
+
+        echo "Wind: $prediction\n";
         $this->assertTrue(true, 'I don\'t know how to test it');
     }
 
